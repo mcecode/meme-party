c5c0f83bfe49d6751edc3bca6e46da135fa5e41d
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2020-06-11 18:36:46 +0800

    modularization 6
    - landing->main.scss = changed element selectors to class selectors
    - landing->index.js = added landing content and style; began adding animation

--- src/component-landing/index.js
+++ src/component-landing/index.js
@@ -4,40 +4,85 @@ import heavyBreathingCat from './images/heavy-breathing-cat.jpg';
 class Landing {
   constructor() {
     this.container = document.createElement('header');
-    this.h1 = document.createElement('h1');
-    this.par = document.createElement('p');
-    this.img = document.createElement('img');
-    this.button = document.createElement('button');
-    this.mount();
-    this.setContent();
+    this.heading = document.createElement('h1');
+    this.paragraph = document.createElement('p');
+    this.catImage = document.createElement('img');
+    this.showButton = document.createElement('button');
+    this.init();
   }
 
-  /**
-   * TODO:
-   * - set setContent
-   * - set h1
-   * - add events tp be handled by handle event
-   */
+  init() {
+    this.mount();
+    this.addStyles();
+    this.setContent();
+    window.addEventListener('load', this);
+    window.addEventListener('resize', this);
+  }
 
   mount() {
     this.container.append(
-      this.h1,
-      this.par,
-      this.img,
-      this.button
-    )
+      this.heading,
+      this.paragraph,
+      this.catImage,
+      this.showButton
+    );
+  }
+
+  addStyles() {
+    this.container.className = style.container;
+    this.heading.className = style.heading;
+    this.paragraph.className = style.paragraph;
+    this.catImage.className = style.catImage;
+    this.showButton.className = style.showButton;
   }
 
   setContent() {
-
+    this.setHeadingContent();
+    this.paragraph.textContent = 'good memes rayt here';
+    this.catImage.src = heavyBreathingCat;
+    this.showButton.textContent = 'dank memes hur';
   }
 
-  setH1Content() {
-
+  setHeadingContent() {
+    const height = window.innerHeight;
+    const width = window.innerWidth;
+    
+    if (height > width) {
+      this.heading.innerHTML = `
+        <pre> Memes </pre>
+        <pre> Purr </pre>
+        <pre> You </pre>`;
+    } else {
+      this.heading.innerHTML = `
+        <pre> Memes Purr You </pre>`;
+    }    
   }
 
-  handleEvent(event) {
+  // ! clean this up
+  // ! make it work
+  setAnimation() {    
+    if (!localStorage.getItem('saw landing animation'))  {
+      this.paragraph.style.transform = 'translateY(-100vh)';
+      this.catImage.style.transform = 'scale(0)';
+      this.showButton.style.transform = 'translateY(100vh)';
+  
+      this.heading.classList.add('headingAnimate');
+      setTimeout(() => {this.paragraph.classList.add('paragraphAnimate');}, 750);
+      setTimeout(() => {this.catImage.classList.add('catImageAnimate');}, 750*2);
+      setTimeout(() => {this.showButton.style.transform = 'scale(1)';}, 750*3)
+      setTimeout(() => {this.showButton.classList.add('showButtonAnimate');}, 750*4);
+  
+      localStorage.setItem('saw landing animation', true);
+    } else {
+      this.showButton.style.animation = '750ms ease-in-out 0ms 5 normal backwards running rippleAnimation';
+    }
+    // try removing this then test
+    window.removeEventListener('load', this);
+  }
 
+  handleEvent(e) {
+    if (e.target === window && e.type === 'load') this.setAnimation();
+    if (e.target === window && e.type === 'resize') this.setHeadingContent();
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -14,9 +14,14 @@ root.append(
 
 if (module.hot) {
   module.hot.accept('./component-landing/index.js', () => {
+    landing.container.remove();
     landing = new Landing();
+    // change to prepend when meme.container is set
+    root.append(landing.container);
   });
   module.hot.accept('./component-meme/index.js', () => {
+    // add remove here
     meme = new Meme();
+    // add append here
   });
 }
