3dbbd54aba4a645f07b02d39f5da53eeec55d3b4
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2020-06-28 16:30:05 +0800

    configs 4
    - created: webpack-log-config.js
    
    - webpack-log-config.js: transferred here the logs to see configs
    - webpack.config.js:
      - cleaned up a bit
      - added a conditional to export using SEE_RUN env var
    - package.json:
      - renamed test scripts to see as it is more fitting for what they do
      - added SEE_RUN to env vars in see scripts
      - removed --config in start and build scripts
    - package-lock.json
      - upped version to 2.0.0 (previously failed to do this)

--- webpack.config.js
+++ webpack.config.js
@@ -340,45 +340,15 @@ config.module = {
 };
 
 /** export webpack configs */
-// module.exports = config;
-
-/**************************************************\
-  testing logs beyond
-\**************************************************/
-const { EOL } = require('os');
-
-// just to break up from top
-console.log(EOL);
-
-// see whole config
-console.log(
-  // bgcyan, fgblack, string, reset, regular object
-  '\x1b[44m \x1b[30m %s \x1b[0m %O',
-  'config = ',
-  config,
-  EOL
-);
-
-// see plugin options
-console.log(
-  // bgcyan, fgblack, string, reset, full object
-  '\x1b[44m \x1b[30m %s \x1b[0m %o',
-  'plugins = ', 
-  {
+if (process.env.SEE_RUN === 'true') {
+  module.exports = [
+    config,
     htmlWebpackPluginOptions,
     miniCssExtractPluginOptions,
     webpackManifestPluginOptions,
     webpackAssetsManifestOptions,
     removePluginOptions
-  },
-  EOL
-);
-
-// see removePluginOptions method
-console.log(
-  // bgcyan, fgblack, string, reset, string
-  '\x1b[44m \x1b[30m %s \x1b[0m %s',
-  'removePluginOptions.before.test[0].method = ',
-  removePluginOptions.before.test[0].method,
-  EOL
-);
+  ]
+} else {
+  module.exports = config;
+}
